b69f046c53f6140be707a8eb1ad79b567a454ed6
joda17 <joda17@wp.pl>
2015-06-12 23:01:52 +0200

    animation test

--- menu/js/deveditor.js
+++ menu/js/deveditor.js
@@ -1,5 +1,38 @@
+var devEditor;
 window.onload = function(){
-	var menuBox = document.getElementById("menu");
-	var editorBox = document.getElementById("editor");
-	var navbarBox = document.getElementById("navbar");
+	devEditor = new DevEditor(document.getElementById("menu"), document.getElementById("editor"), document.getElementById("navbar"));
+}
+
+function DevEditor(menuBox, editorBox, navbarBox){
+	var BOXTYPE = this.BOXTYPE = {MENU:0, NAVBAR:1, EDITOR:2};
+	var POSITION = this.POSITION = {EDITOR:0, MENU: 1};
+	var boxes = this.boxes = [];
+	this.pos = POSITION.MENU;
+	boxes[BOXTYPE.MENU] = menuBox;
+	boxes[BOXTYPE.EDITOR] = editorBox;
+	boxes[BOXTYPE.NAVBAR] = navbarBox;
+}
+DevEditor.prototype.setPosition = function(pos){
+	var boxes = this.boxes;
+	var BOXTYPE = this.BOXTYPE;
+	if(this.pos==pos)return;
+	var dir = "top";
+	if(pos==this.POSITION.MENU){
+		dir = "bottom";
+		console.log(t);
+	}
+	boxes[BOXTYPE.EDITOR].style.animation = "2s cubic-bezier(0.68,-0.55,0.27,1.55) 0s normal none infinite running editor-to-" + dir;
+	boxes[BOXTYPE.NAVBAR].style.animation = "2s cubic-bezier(0.68,-0.55,0.27,1.55) 0s normal none infinite running navbar-to-" + dir;
+	var t = this;
+	setTimeout(function(){
+		boxes[BOXTYPE.EDITOR].style.animation = "";
+		boxes[BOXTYPE.NAVBAR].style.animation = "";
+		boxes[BOXTYPE.EDITOR].classList.remove("top");
+		boxes[BOXTYPE.EDITOR].classList.remove("bottom");
+		boxes[BOXTYPE.NAVBAR].classList.remove("top");
+		boxes[BOXTYPE.NAVBAR].classList.remove("bottom");
+		boxes[BOXTYPE.EDITOR].classList.add(dir);
+		boxes[BOXTYPE.NAVBAR].classList.add(dir);
+		t.pos = pos;
+	}, 2000);
 }
