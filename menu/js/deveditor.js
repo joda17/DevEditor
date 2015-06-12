9af9154aa103ec0f595cb2717b94a855798e3796
joda17 <joda17@wp.pl>
2015-06-12 22:16:11 +0200

    test animations

---
+++ menu/js/deveditor.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+	var menuBox = document.getElementById("menu");
+	var editorBox = document.getElementById("editor");
+	var navbarBox = document.getElementById("navbar");
+}
